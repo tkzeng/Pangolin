Bug fixes, new outputs + score calculation

pangolin/pangolin.py
@@ -37,12 +37,12 @@ def compute_score(ref_seq, alt_seq, strand, d, models):
         alt_seq = alt_seq.to(torch.device("cuda"))
 
     pangolin = []
-    for j in range(8):
+    for j in range(4):
         score = []
-        for model in models[2*j:2*j+2]:
+        for model in models[3*j:3*j+3]:
             with torch.no_grad():
-                ref = model(ref_seq)[0][[1,2,4,5,7,8,10,11][j],:].cpu().numpy()
-                alt = model(alt_seq)[0][[1,2,4,5,7,8,10,11][j],:].cpu().numpy()
+                ref = model(ref_seq)[0][[1,4,7,10][j],:].cpu().numpy()
+                alt = model(alt_seq)[0][[1,4,7,10][j],:].cpu().numpy()
                 if strand == '-':
                     ref = ref[::-1]
                     alt = alt[::-1]
@@ -53,7 +53,7 @@ def compute_score(ref_seq, alt_seq, strand, d, models):
                 score.append(alt-ref)
         pangolin.append(np.mean(score, axis=0))
 
-    pangolin = np.array([pangolin[0]+pangolin[1],pangolin[2]+pangolin[3],pangolin[4]+pangolin[5],pangolin[6]+pangolin[7]])/2
+    pangolin = np.array(pangolin)
     loss = pangolin[np.argmin(pangolin, axis=0), np.arange(pangolin.shape[1])]
     gain = pangolin[np.argmax(pangolin, axis=0), np.arange(pangolin.shape[1])]
     return loss, gain
@@ -132,25 +132,51 @@ def process_variant(lnum, chr, pos, ref, alt, gtf, models, args):
             ((genes_pos,loss_pos,gain_pos),(genes_neg,loss_neg,gain_neg)):
         for gene, positions in genes.items():
             positions = np.array(positions)
-            if args.mask == "True":
-                positions = positions-(pos-d)
-                if len(alt_seq)>len(ref_seq):
-                    positions[positions>d] += (len(alt_seq)-len(ref_seq))
-                positions = positions[(positions>=0) & (positions<len(loss))]
-                # set splice gain at annotated sites to 0
-                gain[positions] = np.minimum(gain[positions], 0)
-                # set splice loss at unannotated sites to 0
-                loss[-positions] = np.maximum(loss[-positions], 0)
+            positions = positions - (pos - d)
+            if len(alt_seq) > len(ref_seq):
+                positions[positions > d] += (len(alt_seq) - len(ref_seq))
 
-            if cutoff != None:
+            if args.mask == "True":
+                positions_filt = positions[(positions>=0) & (positions<len(loss))]
+                # set splice gain at annotated sites to 0
+                gain[positions_filt] = np.minimum(gain[positions_filt], 0)
+                # set splice loss at unannotated sites to 0
+                not_positions = ~np.isin(np.arange(len(loss)), positions_filt)
+                loss[not_positions] = np.maximum(loss[not_positions], 0)
+
+            if args.score_exons == "True":
+                scores1 = gene+'_sites1|'
+                scores2 = gene+'_sites2|'
+
+                for i in range(len(positions)//2):
+                    p1, p2 = positions[2*i], positions[2*i+1]
+                    if p1<0 or p1>=len(loss):
+                        s1 = "NA"
+                    else:
+                        s1 = [loss[p1],gain[p1]]
+                        s1 = np.round(s1[np.argmax(np.abs(s1))],2)
+                    if p2<0 or p2>=len(loss):
+                        s2 = "NA"
+                    else:
+                        s2 = [loss[p2],gain[p2]]
+                        s2 = np.round(s2[np.argmax(np.abs(s2))],2)
+                    if s1 == "NA" and s2 == "NA":
+                        continue
+                    scores1 += "%s:%s|" % (p1-d, s1)
+                    scores2 += "%s:%s|" % (p2-d, s2)
+
+                scores = scores+scores1+scores2
+
+            elif cutoff != None:
+                scores = scores+gene+'|'
                 l, g = np.where(loss<=-cutoff)[0], np.where(gain>=cutoff)[0]
-                scores = gene
                 for p, s in zip(np.concatenate([g-d,l-d]), np.concatenate([gain[g],loss[l]])):
-                    scores = "%s|%s:%s|" % (scores, p, round(s,2))
-                    
+                    scores += "%s:%s|" % (p, round(s,2))
+
             else:
+                scores = scores+gene+'|'
                 l, g = np.argmin(loss), np.argmax(gain),
-                scores = scores+"%s|%s:%s|%s:%s|" % (gene, g-d, round(gain[g],2), l-d, round(loss[l],2))
+                scores += "%s:%s|%s:%s|" % (g-d, round(gain[g],2), l-d, round(loss[l],2))
 
     return scores.strip('|')
 
@@ -165,6 +191,8 @@ def main():
     parser.add_argument("-m", "--mask", default="False", choices=["False","True"], help="If True, splice gains (increases in score) at annotated splice sites and splice losses (decreases in score) at unannotated splice sites will be set to 0.")
     parser.add_argument("-s", "--score_cutoff", type=float, help="Output all sites with absolute predicted change in score >= cutoff, instead of only the maximum loss/gain sites.")
     parser.add_argument("-d", "--distance", type=int, default=50, help="Number of bases on either side of the variant for which splice scores should be calculated. (Default: 50)")
+    parser.add_argument("--score_exons", default="False", choices=["False","True"], help="Output changes in score for both splice sites of annotated exons, as long as one splice site is within the considered range (specified by -d). "
+                                                                                         "Output will be: gene|site1_pos:score|site2_pos:score|...")
     args = parser.parse_args()
 
     variants = args.variant_file
@@ -181,8 +209,8 @@ def main():
         print("Using CPU")
 
     models = []
-    for i in range(8):
-        for j in range(1,3):
+    for i in [0,2,4,6]:
+        for j in range(1,4):
             model = Pangolin(L, W, AR)
             if torch.cuda.is_available():
                 model.cuda()
@@ -218,7 +246,7 @@ def main():
 
     elif variants.endswith(".csv"):
         col_ids = args.column_ids.split(',')
-        variants = pd.read_csv(variants, header=0)#, usecols=col_ids)
+        variants = pd.read_csv(variants, header=0)
         fout = open(args.output_file+".csv", 'w')
         fout.write(','.join(variants.columns)+',Pangolin\n')
         fout.flush()