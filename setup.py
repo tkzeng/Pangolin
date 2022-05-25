0cad895634fe22535482aa31b07acff5baa34763
Tony Zeng <tkzeng@gmail.com>
2022-05-25 14:17:06 -0700

    add option to score exons

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pangolin",
-    version="1.0.1",
+    version="1.0.2",
     author="Tony Zeng",
     author_email="tkyzeng@gmail.com",
     description="Pangolin",
